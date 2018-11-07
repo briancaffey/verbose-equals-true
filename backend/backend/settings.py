3f94a2dfcd50196b3615098633ed4a709a7379fc
briancaffey <briancaffey2010@gmail.com>
2018-11-06 21:29:44 -0500

    added allowed host for docker-compose.dev.yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')1wm*k=x2_2+mm_)3-^q%(4^7m02vh9fprrkq784d=9($0ln@g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
