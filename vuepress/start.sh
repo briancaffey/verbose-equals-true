7c4882c76be000c57cd49e9af750915d05bf9524
briancaffey <briancaffey2010@gmail.com>
2018-12-22 18:35:22 -0500

    debug vuepress [skip ci]

--- vuepress/start.sh
+++ vuepress/start.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# build vuepress files
-yarn run docs:build
-
 # https://docs.npmjs.com/cli/cache
 npm cache verify
 
 # install project dependencies
 npm install
 
+# build vuepress files
+yarn run docs:build
+
 npm ls
 
 pwd
 
 ls
+
+yarn add http-server
 
 # run the development server
 http-server /app/docs/.vuepress/dist -p 8002
