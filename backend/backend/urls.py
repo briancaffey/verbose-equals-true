386e2988ab5283557c08e0bd62b1750a6d403e46
briancaffey <briancaffey2010@gmail.com>
2019-02-06 20:31:10 -0500

    fixed cmd in backend, added static urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,3 +28,8 @@ urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
+
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
